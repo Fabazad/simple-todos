6ff150a3b58dd6a13f775b56cbdcf0ed9128cb87
Fabien <fabien.turgut@etu.umontpellier.fr>
2018-04-21 20:10:06 +0200

    Tuto finished todo list

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
  
 import { Tasks } from '../api/tasks.js';
  
@@ -12,11 +14,18 @@ export default class Task extends Component {
   deleteThisTask() {
     Meteor.call('tasks.remove', this.props.task._id);
   }
+
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+  }
  
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
  
     return (
       <li className={taskClassName}>
@@ -30,7 +39,13 @@ export default class Task extends Component {
           checked={!!this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
- 
+ 		
+		{ this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public' }
+          </button>
+        ) : ''}
+
         <span className="text"><strong>{this.props.task.username ? this.props.task.username : "Unknown"}</strong>: {this.props.task.text}</span>
       </li>
     );
